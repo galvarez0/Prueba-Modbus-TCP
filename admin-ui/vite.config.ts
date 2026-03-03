d74cfab08477eb8b6608a6628e68ead0cdf76b32
galvarez0 <109755988+galvarez0@users.noreply.github.com>
2026-03-03 22:00:53 +0000

    Solución de bugs en ui y archivos modules

--- admin-ui/vite.config.ts
+++ admin-ui/vite.config.ts
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
+  server: { port: 5173, host: true },
     // If you want to run UI locally while calling dockerized admin-api:
     // set VITE_API_BASE to http://localhost:8095
-  },
 })
